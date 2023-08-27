9fec459665e75aea05fcd18c64c68e1bc348c2b0
diegotocchetto <diego.tocchetto@gmail.com>
2023-08-27 19:07:49 -0300

    3ER ENTREGA v10

--- src/routes/mail.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const mailRoutes = express.Router();
-import { isLogged, isUser } from '../middlewares/auth.js';
-import mailController from '../controllers/mail.controller.js/index.js';
-
-mailRoutes.get('/', checkLogin, isUser, mailController.getMail);
-mailRoutes.post('/send', checkLogin, isUser, mailController.sendMail);
-
-module.exports = mailRoutes;
